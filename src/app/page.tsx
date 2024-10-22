d946c5e8ce87dd0900427ae3b9eebab8fd777139
ali <alibenhenia1@gmail.com>
2024-10-22 17:35:44 +0100

    feat:import it i main page route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-
+import TaskManager from "../../components/TaskManager";
 
 
 export default function Home() {
   return (
     <div className="container">
-       
+        <TaskManager />
     </div>
   );
 }
